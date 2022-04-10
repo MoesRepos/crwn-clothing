f7af5a7777b438255d89ac3694e3814bdb7d8911
mhassan1989 <74423643+mhassan1989@users.noreply.github.com>
2022-04-10 10:24:30 -0400

    created sign up component, the form input component, button component

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,30 @@
-import { signInWIthGooglePopup, createUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils'
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+
+import { auth, signInWIthGooglePopup, signInWithGoogleRedirect ,createUserDocumentFromAuth } from '../../../utils/firebase/firebase.utils'
+import SignUpForm from '../../sign-up-form/sign-up-form-component';
 
 const SignIn = () => {
+    // This code covers signing in with google redirect
+    // useEffect(async () =>{    
+    //     const response = await getRedirectResult(auth);
+    //     console.log(response)
+    //     if (response) {
+    //         const userDocRef = await createUserDocumentFromAuth(response.user);
+    //     }
+    // }, []);
     const logGoogleUser = async () => {
         const { user } = await signInWIthGooglePopup();
-        const userDocRef = createUserDocumentFromAuth(user)
+        const userDocRef = await createUserDocumentFromAuth(user)
     };
+
+
     return(
         <div>
             <h1>Sign In Page</h1>
             <button onClick={ logGoogleUser }>Sign in with Google Popup</button> 
+            {/* <button onClick={ signInWithGoogleRedirect }>Sign in with Google Redirect</button>  */}
+            <SignUpForm />
         </div>
     );
 
